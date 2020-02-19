e2753de91819598f83c83619251b2c4436fcb6b3
MstXy <cz1627@nyu.edu>
2020-02-19 13:58:44 +0800

    change the input box

--- my-work/week1/coding-foundation/script.js
+++ my-work/week1/coding-foundation/script.js
@@ -5,12 +5,13 @@ function loaded(){
 
 function display_squares(){
   let square_container = document.getElementById('square_container');
-  document.getElementById('input_box').innerHTML = '';
   // a loop to draw all the squares
   for (var i = 0; i < document.getElementById('input_box').value; i++) {
     let square = document.createElement('p');
     square.className = "square";
     square_container.appendChild(square);
   }
+  document.getElementById('input_box').value = '';
+
 
 }
